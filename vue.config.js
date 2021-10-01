bbeb6d0815dc7c954ebd021c9ed2842078a6e00d
Patrick-web <pntxall100@gmail.com>
2021-10-01 08:31:35 +0300

    changed pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-  devServer: {
-    disableHostCheck: true
-  },
-  transpileDependencies: [
-    'vuetify'
-  ],
+	devServer: {
+		disableHostCheck: true,
+	},
+	transpileDependencies: ["vuetify"],
 
-  publicPath: '',
+	publicPath: "",
 
-  pluginOptions: {
-    cordovaPath: 'src-cordova'
-  }
-}
-
+	pluginOptions: {
+		cordovaPath: "src-cordova",
+	},
+	pwa: {
+		name: "Donors X",
+		themeColor: "#DC143C",
+		msTileColor: "#000000",
+		icons: {
+			favicon32: "img/icons/favicon-32x32.png",
+			favicon16: "img/icons/favicon-16x16.png",
+			appleTouchIcon: "img/icons/apple-icon-152x152.png",
+			msTileImage: "img/icons/ms-icon-144x144.png",
+		},
+	},
+};
